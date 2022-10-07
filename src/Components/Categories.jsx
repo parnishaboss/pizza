d1e809445c956a4791fc4cbcb5de3c021c81b5c5
parnishaboss <zhora0@mail.ru>
2022-10-07 23:12:01 +0300

    4 video not ended

--- src/Components/Categories.jsx
+++ src/Components/Categories.jsx
@@ -1,15 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 export const Categories = ( ) => {
+    const [activeCategory, setActiveCategory] = useState(2)
+    const onClickCategory = (index) => {
+        setActiveCategory(index)
+    }
     return (
         <div className="categories">
             <ul>
-                <li className="active">Все</li>
-                <li>Мясные</li>
-                <li>Вегетарианская</li>
-                <li>Гриль</li>
-                <li>Острые</li>
-                <li>Закрытые</li>
+                <li onClick={() => onClickCategory(0)} className={activeCategory === 0 ? 'active' : ''}>Все</li>
+                <li onClick={() => onClickCategory(1)} className={activeCategory === 1 ? 'active' : ''}>Мясные</li>
+                <li onClick={() => onClickCategory(2)} className={activeCategory === 2 ? 'active' : ''}>Вегетарианская</li>
+                <li onClick={() => onClickCategory(3)} className={activeCategory === 3 ? 'active' : ''}>Гриль</li>
+                <li onClick={() => onClickCategory(4)} className={activeCategory === 4 ? 'active' : ''}>Острые</li>
+                <li onClick={() => onClickCategory(5)} className={activeCategory === 5 ? 'active' : ''}>Закрытые</li>
             </ul>
         </div>
     )
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,20 +1,22 @@
 import React from 'react';
+import pizzaLogo from '../assets/img/pizza-logo.svg'
+import cart from '../assets/img/cart.svg'
 
 export const Header = () => {
     return (
         <div className="header">
             <div className="container">
                 <div className="header__logo">
-                    <img width="38" src="./img/pizza-logo.svg" alt="Pizza logo"/>
+                    <img width="38" src={pizzaLogo} alt="Pizza logo"/>
                     <div>
                         <h1>React Pizza</h1>
                         <p>самая вкусная пицца во вселенной</p>
                     </div>
                 </div>
                 <div className="header__cart">
-                    <a href="/cart.html" className="button button--cart">
+                    <a href={cart} className="button button--cart">
                         <span>520 ₽</span>
-                        <div className="button__delimiter"></div>
+                        <div className="button__delimiter"/>
                         <svg
                             width="18"
                             height="18"
